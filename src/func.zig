529a788f54e980ce99f618855d3a654e2cca164e
Christoph Voigt <voigt.christoph@gmail.com>
2022-07-05 09:40:55 +0200

    implement Func.call

--- src/func.zig
+++ src/func.zig
@@ -11,7 +11,8 @@ pub const wasm = @import("wasm");
 
 const log = std.log.scoped(.wasmtime_zig);
 
-pub const ByteVec = wasm.ByteVec;
+pub const ValVec = wasm.ByteVec;
+pub const Trap = wasm.Trap;
 
 pub const Func = struct {
     inner: *wasm.Func,
@@ -21,6 +22,80 @@ pub const Func = struct {
     pub fn init(store: *Store, callback: anytype) !Func {
         return Func{
             .inner = try wasm.Func.init(store.inner, callback),
+        };
+    }
+
+    pub fn call(self: Func, store: *Store, comptime ResultType: type, args: anytype) CallError!ResultType {
+        if (!comptime trait.isTuple(@TypeOf(args)))
+            @compileError("Expected 'args' to be a tuple, but found type '" ++ @typeName(@TypeOf(args)) ++ "'");
+
+        const args_len = args.len;
+        comptime var wasm_args: [args_len]Value = undefined;
+        inline for (wasm_args) |*arg, i| {
+            arg.* = switch (@TypeOf(args[i])) {
+                i32, u32 => .{ .kind = .i32, .of = .{ .i32 = @bitCast(i32, args[i]) } },
+                i64, u64 => .{ .kind = .i64, .of = .{ .i64 = @bitCast(i64, args[i]) } },
+                f32 => .{ .kind = .f32, .of = .{ .f32 = args[i] } },
+                f64 => .{ .kind = .f64, .of = .{ .f64 = args[i] } },
+                *Func => .{ .kind = .funcref, .of = .{ .ref = args[i] } },
+                *Extern => .{ .kind = .anyref, .of = .{ .ref = args[i] } },
+                else => |ty| @compileError("Unsupported argument type '" ++ @typeName(ty) + "'"),
+            };
+        }
+
+        // TODO multiple return values
+        const result_len: usize = if (ResultType == void) 0 else 1;
+        if (result_len != wasm_func_result_arity(self.inner)) return CallError.InvalidResultCount;
+        if (args_len != wasm_func_param_arity(self.inner)) return CallError.InvalidParamCount;
+
+        const final_args = ValVec{
+            .size = args_len,
+            .data = if (args_len == 0) undefined else @ptrCast([*]Value, &wasm_args),
+        };
+
+        var trap: ?*wasm.Trap = null;
+        var result_list = ValVec.initWithCapacity(result_len);
+        defer result_list.deinit();
+
+        const err = wasmtime_func_call(store.inner, // wasmtime_context_t *store,
+            self.inner, // const wasmtime_func_t *func,
+            &final_args, // const wasmtime_val_t *args,
+            final_args.size, // size_t nargs,
+            &result_list, // wasmtime_val_t *results,
+            result_list.size, // size_t nresults,
+            &trap // wasm_trap_t **trap
+        );
+
+        if (err) |e| {
+            defer e.deinit();
+            var msg = e.getMessage();
+            defer msg.deinit();
+
+            log.err("Unable to call function: '{s}'", .{msg.toSlice()});
+            return CallError.InnerError;
+        }
+
+        if (trap) |t| {
+            t.deinit();
+            // TODO handle trap message
+            log.err("code unexpectedly trapped", .{});
+            return CallError.Trap;
+        }
+
+        if (ResultType == void) return;
+
+        // TODO: Handle multiple returns
+        const result_ty = result_list.data[0];
+        if (!wasm.Func.matchesKind(ResultType, result_ty.kind)) return CallError.InvalidResultType;
+
+        return switch (ResultType) {
+            i32, u32 => @intCast(ResultType, result_ty.of.i32),
+            i64, u64 => @intCast(ResultType, result_ty.of.i64),
+            f32 => result_ty.of.f32,
+            f64 => result_ty.of.f64,
+            *Func => @ptrCast(?*Func, result_ty.of.ref).?,
+            *Extern => @ptrCast(?*Extern, result_ty.of.ref).?,
+            else => |ty| @compileError("Unsupported result type '" ++ @typeName(ty) ++ "'"),
         };
     }
 
@@ -71,4 +146,6 @@ pub const Func = struct {
         usize, // size_t nresults,
         *wasm.Trap, // wasm_trap_t **trap
     ) ?*WasmError;
+    extern "c" fn wasm_func_result_arity(*const wasm.Func) usize;
+    extern "c" fn wasm_func_param_arity(*const wasm.Func) usize;
 };
